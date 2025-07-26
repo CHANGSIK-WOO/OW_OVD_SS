4147c278b53df7a7cf93906acf8808095262b504
ChangSik Woo <jasoncswoo@gmail.com>
2025-07-26 16:08:07 +0900

    14th commit : add SemanticSegHead in dense_heads/__init__.py

--- yolo_world/models/dense_heads/__init__.py
+++ yolo_world/models/dense_heads/__init__.py
@@ -1,14 +1,15 @@
 # Copyright (c) Tencent Inc. All rights reserved.
 from .yolo_world_head import YOLOWorldHead, YOLOWorldHeadModule, RepYOLOWorldHeadModule
 from .yolo_world_seg_head import YOLOWorldSegHead, YOLOWorldSegHeadModule
+from .yolo_world_seg_head_semantic import YOLOWorldSemanticSegHead, YOLOWorldSemanticSegHeadModule # 25.07.26 add for semantic segmentation
 from .fomo_head import FOMOHead, FOMOHeadModule
 from .umb_head import UMBHead, UMBHeadModule
 from .fomo_nobn_head import FOMOnoBNHead, FOMOnoBNHeadModule
 from .our_head import OurHead, OurHeadModule
 
 __all__ = [
-    'YOLOWorldHead', 'YOLOWorldHeadModule', 'YOLOWorldSegHead',
-    'YOLOWorldSegHeadModule', 'RepYOLOWorldHeadModule',
+    'YOLOWorldHead', 'YOLOWorldHeadModule', 'YOLOWorldSegHead', 'YOLOWorldSegHeadModule', 
+    'YOLOWorldSemanticSegHead', 'YOLOWorldSemanticSegHeadModule', 'RepYOLOWorldHeadModule',
     'FOMOHead', 'FOMOHeadModule', 'UMBHead', 'UMBHeadModule',
     'FOMOnoBNHead', 'FOMOnoBNHeadModule', 'OurHead', 'OurHeadModule'
 ]
